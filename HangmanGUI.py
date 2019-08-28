46ca1a3f5e5c3b7b52ab7908ee9735e3cea09b0e
Vihaan Das <dvihaan1@gmail.com>
2019-08-28 13:17:08 -0700

    Hangman

--- HangmanGUI.py
+++ HangmanGUI.py
@@ -8,7 +8,8 @@ class Game:
 	
 	def __init__(self, root):
 		root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
-		self.word = StringVar()
+		self.word = ''
+		
 		self.letterGuesses = StringVar()
 		self.guess = StringVar()
 		self.guess.trace('w',self.limitEntry)
@@ -24,14 +25,16 @@ class Game:
 		Label(frame, textvariable=self.lives).grid(row=2, column=1)
 		Label(frame, textvariable=self.result).grid(row=3, columnspan=2)
 		
-		self.word.set(self.wordPick())
+		self.word = self.wordPick()
 		self.printWord()
 		#self.game(self.wordPick())
 	
 	def limitEntry(self, *args):
 		value = self.guess.get()
+		print(value)
 		if len(value)>1:
 			self.guess.set(value[-1:])
+			
 		
 	def getWords(self):
 		u = 'https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/'
@@ -47,89 +50,37 @@ class Game:
 		return wordList
 		
 	def wordPick(self):
-		self.words = self.getWords()
-		return random.choice(self.words)
+		self.wordList = self.getWords()
+		return random.choice(self.wordList)
 				
 	def printWord(self):
 		wordDisplay = ''
 		letterGuesses = self.letterGuesses.get()
-		word = self.word.get()
-		for letter in word:
+		for letter in self.word:
 			if letterGuesses.find(letter) > -1:
 				wordDisplay = wordDisplay + letter
 			else:
 				wordDisplay = wordDisplay + '-'
 		self.letterGuesses.set(wordDisplay)
     				
-	def game(self, word):
-		while True:
-			guess = get_guess(word)
-			if process_guess(guess, word):
-				print(word)
-				print('You Win!!!')
-				break
-			if lives == 0:
-				print('You got hung!...')
-				print('the word was '+word)
-				break
-				
-
-
-def get_guess(word):
-    printWord(word)
-    print('Lives remaining: ' + str(lives))
-    print('Incorrect Guesses: ' + str(wrongGuesses))
-    guess = ''
-    while True:
-        guess = input('Guess a single letter: ')
-        if len(guess) == 1:
-            break
-    return guess
-    
-
-    
-def process_guess(guess, word):
-    '''
-    if your guess is one of the letters in the word, then change the
-    blank space mapping to that letter in the dealer's word to the
-    letter you guessed.
-    
-    if the letter you guessed is not one of the letters in the word,
-    then one of your lives is removed and your remaining life count (global)
-    is decreased by 1.
-    '''
-    global letterGuesses
-    global lives
-    global wrongGuesses
-    
-    if word.find(guess) < 0:
-        lives = lives - 1
-        wrongGuesses.append(guess)
-        return False
-    
-    letterGuesses = letterGuesses + guess
-    
-    
-    return gameEnd(word)
-    
-def gameEnd(word):
-    '''
-    if the length of the unique letters in the word is equal to
-    the length of letterGuesses, then return True
-    '''
-    if len(letterGuesses) == len(set(word)):
-        return True
-    return False
-    
-
-'''
-wrongGuesses = []
-lives = 10
-letterGuesses = ''
-word = wordPick() 
-game(word)
-'''
-
+	def process_guess(self):
+		'''
+		if your guess is one of the letters in the word, then change the
+		blank space mapping to that letter in the dealer's word to the
+		letter you guessed.
+		
+		if the letter you guessed is not one of the letters in the word,
+		then one of your lives is removed and your remaining life count (global)
+		is decreased by 1.
+		'''
+		guess = self.guess.get()		
+		
+		if self.word.find(guess) < 0:
+			self.lives.set(self.lives.get() -1)
+		else:
+			self.letterGuesses.set(self.letterGuesses.get()+guess)
+		
+		
 root = Tk()
 root.wm_title('Hangman')
 game = Game(root)
