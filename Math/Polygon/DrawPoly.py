858335f4ab7fad359820c6e410fbf463d2bf92de
Vihaan Das <dvihaan1@gmail.com>
2019-12-28 14:50:43 -0800

    added comments

--- Math/Polygon/DrawPoly.py
+++ Math/Polygon/DrawPoly.py
@@ -2,6 +2,17 @@ import sys, pygame
 import time
 import Polygon
 
+'''
+This is an interactive program which creates a polygon of varying shape, size, and rotation
+Controls: 
+Right mouse button = increase # of sides
+Left mouse button = decrease # of sides
+Middle click + Mouse Right = increase side length
+Middle click + Mouse left = decrease side length
+Scroll wheel = rotate shape
+'''
+
+#Initialize PyGame and create a 1280 x 1024 pixel window
 size = width, height = 1280, 1024
 sides = 3
 sideLength = 10
@@ -17,30 +28,38 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill(pygame.Color("white"))
 
+# The function to actually render the polygon
 def renderPolygon():
+    # Creating the global variables for the amount of sides, side length, the rotation angle, and the mouse button being pressed 
     global sides, sideLength, mouseEvent, angle
+    
     if mouseEvent == False:
+        # decreasing the number of sides
         if pygame.mouse.get_pressed() == (1,0,0):
             if sides > 3:
                 sides = sides - 1
                 print("Number of Sides = {}, Side Length = {}".format(sides, sideLength))
             mouseEvent = True
+        # increasing the amount of sides
         if pygame.mouse.get_pressed() == (0,0,1):
             if sides < 1000:
                 sides = sides + 1    
                 print("Number of Sides = {}, Side Length = {}".format(sides, sideLength))
             mouseEvent = True  
+        # Decreasing the side length
         if pygame.mouse.get_pressed() == (1,1,0):
             if sideLength > 10:
                 sideLength = sideLength - 10
                 print("Number of Sides = {}, Side Length = {}".format(sides, sideLength))
             mouseEvent = True
+        # Increasing the side length
         if pygame.mouse.get_pressed() == (0,1,1):
             if sideLength < 100:
                 sideLength = sideLength + 10    
                 print("Number of Sides = {}, Side Length = {}".format(sides, sideLength))
             mouseEvent = True                   
 
+    # Drawing the polygon
     centre = (pygame.mouse.get_pos())
     points = Polygon.GetPoints(sides,sideLength, centre[0], centre[1], angle)
     pygame.draw.lines(background, pygame.Color("black"), False, points)
@@ -49,6 +68,7 @@ def renderPolygon():
     screen.blit(background, (0, 0))
 
 
+# Running the function written before and taking user input
 def run_show():
     global mouseEvent, angle
     while True:        
@@ -62,15 +82,17 @@ def run_show():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+        # Setting the background colour (feel free to edit the string to any colour you like)
         background.fill(pygame.Color("white"))
-
+        
+        # Running the main funtion
         renderPolygon()
 
+        # Setting the window's FPS
         pygame.display.update()
+        # Edit the FPS by changing the input for clock.tick
         clock.tick(250)
         mouseEvent = False
 
+# Finally running the program
 run_show()
-
-
--- Math/Polygon/Polygon.py
+++ Math/Polygon/Polygon.py
@@ -1,23 +1,31 @@
 import math
 
+'''
+This is a function to get the points that  you are supposed to plot, given a certain side length and amount of sides.
+This function and code only prints the points you would need to plot the shape that you have decided on.
+'''
+#The inputs, in order from left to right are: number of sides, side length, centre x position, centre y position, and the angle the shape is rotated
+#By default, the shape is centered at the origin and has no rotation from its original shape
 def GetPoints(n, a, cx = 0, cy = 0, theta = 0):
+    #the list of tuples that will contain the points
     points = []
+    #r = the distance from the centre to any vertex
     r = a*math.sin(math.radians(90-180/n))/math.sin(math.radians(360/n))
+    #creating the tuples containing the points
     for i in range(n+1):
         Vix = r*math.cos(math.radians(i*360/n+theta)) + cx
         Viy = r*math.sin(math.radians(i*360/n+theta)) + cy
         points.append((Vix,Viy))
     return points
-
+#printing the points that should be connected in a drawing
 def Draw(points):
     for p in range(len(points)-1):
         print("({:.2f},{:.2f}) ---------- ({:.2f},{:.2f})".format(points[p][0],points[p][1],points[p+1][0],points[p+1][1]))
+#running the program
 def main():
     points = GetPoints(8,1)
 
     Draw(points)
-
-    #print(format(math.cos(math.radians(90)), '.2f'))
 
 if __name__ == '__main__':
     main()
