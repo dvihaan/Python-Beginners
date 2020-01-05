99c4917ddf47704cfe44c8e6555e94b3306d66c9
Vihaan Das <dvihaan1@gmail.com>
2020-01-05 15:35:08 -0800

    created code

---
+++ Math/Polygon/DrawKoch.py
@@ -0,0 +1,92 @@
+import sys,pygame
+import time
+import Koch
+import math
+
+size = width, height = 1280,1024
+sideLength = 500
+angle = 0
+level = 0
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.display.set_caption('Snowflake')
+pygame.mouse.set_visible(True)
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill(pygame.Color("white"))
+
+def drawEquilateral():
+    global sideLength, angle
+    centre = (pygame.mouse.get_pos())
+    points = Koch.equalVertex(x1 = centre[0]-sideLength/2,y1 = centre[1],x2 = centre[0]+sideLength/2,y2 = centre[1])
+    for i in range(len(points)):        
+        x = points[i][0]-centre[0]
+        y = points[i][1]-centre[1]
+        points[i] = (centre[0]+x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle)), centre[1]+x*math.sin(math.radians(angle)) + y*math.cos(math.radians(angle)))
+    pygame.draw.lines(background, pygame.Color("black"), False, points)
+    screen.blit(background, (0, 0))
+
+def drawKoch():
+    global sideLength, angle, level
+    centre = (pygame.mouse.get_pos())
+    points = Koch.equalVertex(x1 = centre[0]-sideLength/2,y1 = centre[1],x2 = centre[0]+sideLength/2,y2 = centre[1])
+    points = Koch.Snowflake(points,level)
+    for i in range(len(points)):        
+        x = points[i][0]-centre[0]
+        y = points[i][1]-centre[1]
+        points[i] = (centre[0]+x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle)), centre[1]+x*math.sin(math.radians(angle)) + y*math.cos(math.radians(angle)))
+    pygame.draw.lines(background, pygame.Color("black"), False, points)
+    screen.blit(background, (0, 0))
+
+def run_show():
+    global sideLength, angle, level
+    while True:        
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print("Button = {}".format(event.button))
+                print("Pressed = {}".format(pygame.mouse.get_pressed()))
+                
+                if event.button == 1:
+                    
+                    if pygame.mouse.get_pressed() == (1,0,0):
+                        if level > 0:
+                            level -= 1
+                    
+                    if pygame.mouse.get_pressed() == (1,1,0):
+                        if sideLength > 5:
+                            sideLength = sideLength - 5                            
+                if event.button == 3:
+                    
+                    if pygame.mouse.get_pressed() == (0,0,1):
+                        if level < 6:
+                            level += 1  
+                    
+                    if pygame.mouse.get_pressed() == (0,1,1):
+                        if sideLength < 500:
+                            sideLength = sideLength + 5 
+                
+                if event.button == 4:
+                    angle = angle + 50
+                if event.button == 5:
+                    angle = angle - 50
+                
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        # Setting the background colour (feel free to edit the string to any colour you like)
+        background.fill(pygame.Color("white"))
+        
+        # Running the main funtion
+        #drawEquilateral()
+        drawKoch()
+
+        # Setting the window's FPS
+        pygame.display.update()
+        # Edit the FPS by changing the input for clock.tick
+        clock.tick(250)
+
+run_show()
---
+++ Math/Polygon/Koch.py
@@ -0,0 +1,52 @@
+import math
+def equalVertex(x1=-10,y1=0,x2=10,y2=0):
+    x3 = x1/2 + x2/2
+    y3 = y1/2 + y2/2
+    #print("x3={}, y3={}".format(x3, y3))
+    l = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    #print("l={}".format(l))
+    m1 = (y2-y1)/(x2-x1)
+    #print("m1={}".format(m1))
+    c = m1*l*math.sin(math.radians(60))/math.sqrt(m1**2 + 1)
+    #print("c={}".format(c))
+    if x2 == x1:
+         x4 = x3+l*math.sin(math.radians(60))
+    else:
+        x4 = c+x3
+
+    if y2 == y1:
+        y4 = y3-l*math.sin(math.radians(60))
+    else:
+        if x2 == x1:
+            y4 = y3
+        else: 
+            y4 = y3 -c/m1
+
+    points = [(x1,y1),(x4,y4),(x2,y2),(x1,y1)] 
+
+    return points
+
+def Snowflake(points, level = 0):
+
+    while level > 0:
+        newpoints = [points[0]]
+        for i in range(len(points)-1):
+            pt1 = points[i]
+            pt2 = points[i+1]
+            pta = (2*pt1[0]/3+pt2[0]/3,2*pt1[1]/3+pt2[1]/3)
+            ptb = (pt1[0]/3+2*pt2[0]/3,pt1[1]/3+2*pt2[1]/3)
+            calculatedPoints = equalVertex(pta[0],pta[1],ptb[0],ptb[1])
+            for j in range(len(calculatedPoints) - 1):
+                newpoints.append(calculatedPoints[j]) 
+            newpoints.append(pt2)
+        points = newpoints
+        level -= 1
+    return points
+
+def main():
+    point = equalVertex(1149.67, 17.54, 1153.21, 23.2)
+
+    print(point)
+
+if __name__ == '__main__':
+    main()
