d6bfd2d19d809072c107a45f3769284df5a1a613
Vihaan Das <dvihaan1@gmail.com>
2020-01-07 18:58:00 -0800

    Optimization

--- Math/Polygon/DrawKoch.py
+++ Math/Polygon/DrawKoch.py
@@ -3,91 +3,103 @@ import time
 import Koch
 import math
 
-size = width, height = 1280,1024
+size = width, height =1280, 1024
+# Setting the background colour (feel free to edit the string to any colour you like)
+backgroundColor = "Black"
+# Setting the foreground colour (feel free to edit the string to any colour you like)
+foregroundColor = "Green"
 sideLength = 500
 angle = 0
 level = 0
-
+minLevel = 0
+maxLevel = 7
+levelChange = 1
+minLength = 50
+lengthChange = 50
+vertices = {}
 pygame.init()
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption('Snowflake')
 pygame.mouse.set_visible(True)
-
+maxLength = int(screen.get_size()[1]*0.9)
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill(pygame.Color("white"))
 
-def drawEquilateral():
-    global sideLength, angle
-    centre = (pygame.mouse.get_pos())
-    points = Koch.equalVertex(x1 = centre[0]-sideLength/2,y1 = centre[1],x2 = centre[0]+sideLength/2,y2 = centre[1])
-    for i in range(len(points)):        
-        x = points[i][0]-centre[0]
-        y = points[i][1]-centre[1]
-        points[i] = (centre[0]+x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle)), centre[1]+x*math.sin(math.radians(angle)) + y*math.cos(math.radians(angle)))
-    pygame.draw.lines(background, pygame.Color("black"), False, points)
-    screen.blit(background, (0, 0))
+def applyTranslation(point, centre):
+    x = point[0]+centre[0]
+    y = point[1]+centre[1]
+    return (x, y)
 
-def drawKoch():
-    global sideLength, angle, level
-    centre = (pygame.mouse.get_pos())
-    xoff = sideLength/2
-    yoff = sideLength*math.tan(math.radians(60))/6
-    points = Koch.equalVertex(x1 = centre[0]-xoff,y1 = centre[1]+yoff,x2 = centre[0]+xoff,y2 = centre[1]+yoff)
+def applyRotation(point, centre):
+    x = point[0]-centre[0]
+    y = point[1]-centre[1]
+    return (centre[0]+x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle)), centre[1]+x*math.sin(math.radians(angle)) + y*math.cos(math.radians(angle)))
 
-    points = Koch.Snowflake(points,level)
-    for i in range(len(points)):        
-        x = points[i][0]-centre[0]
-        y = points[i][1]-centre[1]
-        points[i] = (centre[0]+x*math.cos(math.radians(angle)) - y*math.sin(math.radians(angle)), centre[1]+x*math.sin(math.radians(angle)) + y*math.cos(math.radians(angle)))
-    pygame.draw.lines(background, pygame.Color("black"), False, points)
+def drawKoch(centre=(0,0), drawLevel=0):
+    global sideLength, angle, level, backgroundColor, foregroundColor
+    if drawLevel == 0:
+        drawLevel = level
+    background.fill(pygame.Color(backgroundColor))
+    if str(sideLength) in vertices and int(vertices[str(sideLength)]["level"]) >= drawLevel:
+        points = vertices[str(sideLength)]["points"]
+        pointLevel = int(vertices[str(sideLength)]["level"])
+        if drawLevel < pointLevel:
+            jump = int(math.pow(4, pointLevel - drawLevel))
+            levelPoints = []
+            for i in range(0, len(points), jump):
+                levelPoints.append(points[i])
+            points = levelPoints
+    else:
+        xoff = sideLength/2
+        yoff = sideLength*math.tan(math.radians(60))/6
+        points = Koch.equalVertex(x1 = 0-xoff,y1 = 0+yoff,x2 = 0+xoff,y2 = 0+yoff)
+        points = Koch.Snowflake(points,drawLevel)
+        vertices[str(sideLength)] = {"level": str(drawLevel), "points": points}
+
+    if centre[0]==0 and centre[1]==0:
+        centre = (pygame.mouse.get_pos())
+    points = list(map(lambda p: applyTranslation(p,centre), points))
+    points = list(map(lambda p: applyRotation(p,centre), points))
+    pygame.draw.lines(background, pygame.Color(foregroundColor), False, points)
     screen.blit(background, (0, 0))
+    pygame.display.update()
 
 def run_show():
-    global sideLength, angle, level
-    while True:        
+    global sideLength, angle, level, minLevel, maxLevel, levelChange, minLength, maxLength, lengthChange, backgroundColor
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-
                 if event.button == 1:
-                    
                     if pygame.mouse.get_pressed() == (1,0,0):
-                        if level > 0:
-                            level -= 1
-                    
+                        if level > minLevel:
+                            level -= levelChange
                     if pygame.mouse.get_pressed() == (1,1,0):
-                        if sideLength > 50:
-                            sideLength = sideLength - 50                           
+                        if sideLength > minLength:
+                            sideLength -= lengthChange
                 if event.button == 3:
-                    
                     if pygame.mouse.get_pressed() == (0,0,1):
-                        if level < 7:
-                            level += 1  
-                    
+                        if level < maxLevel:
+                            level += levelChange
                     if pygame.mouse.get_pressed() == (0,1,1):
-                        if sideLength < 5000:
-                            sideLength = sideLength + 50 
-                
+                        if sideLength < maxLength:
+                            sideLength += lengthChange
                 if event.button == 4:
                     angle = angle + 10
                 if event.button == 5:
                     angle = angle - 10
-                
-
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        # Setting the background colour (feel free to edit the string to any colour you like)
-        background.fill(pygame.Color("white"))
-        
+        background.fill(pygame.Color(backgroundColor))
+
         # Running the main funtion
-        #drawEquilateral()
         drawKoch()
 
         # Setting the window's FPS
-        pygame.display.update()
         # Edit the FPS by changing the input for clock.tick
-        clock.tick(250)
+        clock.tick(60)
 
+#drawKoch((int(screen.get_size()[0]/2), int(screen.get_size()[1]/2)), 7)
 run_show()
--- Math/Polygon/Koch.py
+++ Math/Polygon/Koch.py
@@ -4,39 +4,18 @@ def equalVertex(x1=-10,y1=0,x2=10,y2=0):
     y3 = y1/2 + y2/2
     l = math.sqrt((x1-x2)**2 + (y1-y2)**2)
     m = (y2-y1)/(x2-x1)
-    
-    if y2 != y1:
-        sign = int((y2-y1)/abs(y2-y1))
-    else:
-        sign = int((x2-x1)/abs(x2-x1))
-    
-    mpos = m >= 0   
-    delypos = y2 >= y1
-    addpi = mpos ^ delypos
-    theta = 60
-    if addpi:
-        theta = theta + 180
+    sign = 1 if y2>y1 else -1 if y2<y1 else 1 if x2>x1 else -1 if x2<x1 else 0
+    theta = 240 if (m >= 0) ^ (y2 >= y1) else 60
     c = sign*m*l*math.sin(math.radians(theta))/math.sqrt(m**2 + 1)
-
-    if x2 == x1:
-        x4 = x3 + sign*l*math.sin(math.radians(theta))
-    else:
-        x4 = x3 + sign*c
-
-    if m == 0:
-        y4 = y3 - sign*l*math.sin(math.radians(theta))
-    else:
-        if x2 == x1:
-            y4 = y3*sign
-        else:
-            y4 = y3 - c*sign/m
-
-    points = [(x1,y1),(x4,y4),(x2,y2),(x1,y1)] 
-
+    x4 = x3 + sign*l*math.sin(math.radians(theta)) if x2==x1 else x3 + sign*c
+    y4 = y3 - sign*l*math.sin(math.radians(theta)) if m==0 else y3*sign if x2==x1 else y3 - c*sign/m
+    points = [(x1,y1),(x4,y4),(x2,y2),(x1,y1)]
     return points
 
 def Snowflake(points, level = 0):
-
+    print("Level-{} Snowflake drawn for side length {}".format(level, int(math.sqrt(math.pow(points[0][0] - points[1][0], 2) + math.pow(points[0][1] - points[1][1], 2)))))
+    n = 1
+    print("Level = {}, Points = {}".format(0, len(points)))
     while level > 0:
         newpoints = [points[0]]
         for i in range(len(points)-1):
@@ -46,15 +25,16 @@ def Snowflake(points, level = 0):
             ptb = (pt1[0]/3+2*pt2[0]/3,pt1[1]/3+2*pt2[1]/3)
             calculatedPoints = equalVertex(pta[0],pta[1],ptb[0],ptb[1])
             for j in range(len(calculatedPoints) - 1):
-                newpoints.append(calculatedPoints[j]) 
+                newpoints.append(calculatedPoints[j])
             newpoints.append(pt2)
         points = newpoints
         level -= 1
+        print("Level = {}, Points = {}".format(n, len(newpoints)))
+        n +=1
     return points
 
 def main():
     point = equalVertex(33.33,0, -33.33, 0)
-
     print(point)
 
 if __name__ == '__main__':
