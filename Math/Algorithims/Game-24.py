c96b614f5e638e19e2836b6207894029d64e9fd3
Vihaan Das <dvihaan1@gmail.com>
2020-03-08 15:23:36 -0700

    fixed division by 0

--- Math/Algorithims/Game-24.py
+++ Math/Algorithims/Game-24.py
@@ -14,17 +14,21 @@ def getsol(a,b,trgt,ops):
     
 
 def checktarget(numbers,trgt,ops):
+    print("Numbers = "+str(numbers)+", Target = "+str(trgt)+", Ops = "+ops)
     if len(numbers) == 2:
         getsol(numbers[0],numbers[1],trgt,ops)
-    for i in range(len(numbers)):
-        n = numbers[i]
-        restnumbers = numbers[:i]+numbers[i+1:]
-        checktarget(restnumbers,trgt-n,ops.replace("()", "({}+())".format(n)))
-        checktarget(restnumbers,-trgt-n,ops.replace("()", "({}-())".format(n)))
-        checktarget(restnumbers,trgt+n,ops.replace("()", "(()-{})".format(n)))
-        checktarget(restnumbers,int(trgt/n),ops.replace("()", "(()*{})".format(n)))
-        checktarget(restnumbers,int(n/trgt),ops.replace("()", "(()÷{})".format(n)))
-        checktarget(restnumbers,trgt*n,ops.replace("()", "({}÷()))".format(n)))
+    else:
+        for i in range(len(numbers)):
+            n = numbers[i]
+            restnumbers = numbers[:i]+numbers[i+1:]
+            checktarget(restnumbers,trgt-n,ops.replace("()", "({}+())".format(n)))
+            checktarget(restnumbers,-trgt-n,ops.replace("()", "({}-())".format(n)))
+            checktarget(restnumbers,trgt+n,ops.replace("()", "(()-{})".format(n)))
+            if n != 0:
+                checktarget(restnumbers,int(trgt/n),ops.replace("()", "(()*{})".format(n)))
+            if trgt != 0:
+                checktarget(restnumbers,int(n/trgt),ops.replace("()", "(()÷{})".format(n)))
+            checktarget(restnumbers,trgt*n,ops.replace("()", "({}÷()))".format(n)))
 
 def main():
     nums = [8,1,8,3]
