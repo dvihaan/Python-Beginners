897e18c8b3d5d072b3d0f07ca705ea408f8fd7e0
Vihaan Das <dvihaan1@gmail.com>
2020-03-07 21:22:14 -0800

    Game 24

---
+++ Math/Algorithims/Game-24.py
@@ -0,0 +1,34 @@
+def getsol(a,b,trgt,ops):
+    if a+b == trgt:
+        print(ops.replace("()","({}+{})".format(a,b)))
+    if a-b == trgt:
+        print(ops.replace("()","({}-{})".format(a,b)))
+    if b-a == trgt:
+        print(ops.replace("()","({}-{})".format(b,a)))
+    if a*b == trgt:
+        print(ops.replace("()","({}*{})".format(a,b)))
+    if a/b == trgt:
+        print(ops.replace("()","({}÷{})".format(a,b)))
+    if b/a == trgt:
+        print(ops.replace("()","({}÷{})".format(b,a)))
+    
+
+def checktarget(numbers,trgt,ops):
+    if len(numbers) == 2:
+        getsol(numbers[0],numbers[1],trgt,ops)
+    for i in range(len(numbers)):
+        n = numbers[i]
+        restnumbers = numbers[:i]+numbers[i+1:]
+        checktarget(restnumbers,trgt-n,ops.replace("()", "({}+())".format(n)))
+        checktarget(restnumbers,-trgt-n,ops.replace("()", "({}-())".format(n)))
+        checktarget(restnumbers,trgt+n,ops.replace("()", "(()-{})".format(n)))
+        checktarget(restnumbers,int(trgt/n),ops.replace("()", "(()*{})".format(n)))
+        checktarget(restnumbers,int(n/trgt),ops.replace("()", "(()÷{})".format(n)))
+        checktarget(restnumbers,trgt*n,ops.replace("()", "({}÷()))".format(n)))
+
+def main():
+    nums = [8,1,8,3]
+    checktarget(nums, 24, "()")
+
+if __name__== "__main__":
+  main()
