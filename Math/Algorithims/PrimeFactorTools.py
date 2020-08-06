ff962223410a5d1ccb19eb9021612032e019060d
Vihaan Das <dvihaan1@gmail.com>
2020-08-05 22:15:17 -0700

    added to PrimeFactorTools

--- Math/Algorithims/PrimeFactorTools.py
+++ Math/Algorithims/PrimeFactorTools.py
@@ -5,12 +5,28 @@ def isPrime(x):
         return False
     isit = True
     for p in range(2,math.ceil(math.sqrt(x))+1):
-        print(p)
         if x%p == 0 and x != p:
             isit = False
             break
 
     return isit
 
+def nextPrime(c):
+    i = c+1
+    while True:
+        if isPrime(i) == True:
+            return i
+        i += 1
 
-print(isPrime(1))
+def allFactors(n):
+    factorList = set()
+    for i in range(1, math.ceil(math.sqrt(n))+1):
+        if n%i == 0:
+            factorList.add(int(n/i))
+            factorList.add(i)
+    return list(factorList)
+
+def factorSum(f):
+    return sum(allFactors(f)[:-1])
+
+print(factorSum(6))
